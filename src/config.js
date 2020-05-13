0bd1b73c30e1f123856d0af511d9fc00efe272e5
Lance Brieden <lance.brieden@alchemysystems.com>
2020-05-13 16:56:39 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "lb1-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://h4rcnia4hl.execute-api.us-east-1.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_N0ageHrOR",
+    APP_CLIENT_ID: "j47u17prakdq3m5u7n9gk61id",
+    IDENTITY_POOL_ID: "us-east-1:1ec29833-5a12-4547-a585-333a2ae1a3bb"
+  }
+};
